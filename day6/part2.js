85f907d07c7415a06812d03be7d567955e57fdd6
Arthur Hoek <arthur@arthurhoek.nl>
2021-12-13 15:29:46 +0100

    Day 6 2

--- day6/part2.js
+++ day6/part2.js
@@ -1,22 +1,19 @@
 const fs = require('fs'), filename = process.argv[2];
 fs.readFile(filename, 'utf8', (err, data) => {
-  let fish = data.trim().split(',').join('');
-  for (let day = 1; day <= 150; day++) {
-    console.log(day)
-    let ng = '';
-    let n = 0;
-    for (let i = 0; i < fish.length; i++) {
-      const f = fish[i];
-      if (f === '0') {
-        ng = `${ng}6`;
-        n++;
-      } else {
-        ng = `${ng}${parseInt(f) - 1}`;
-      }
-    }
+  const fish = data.trim().split(',').map(n => parseInt(n));
+  const g = Array(9).fill(0);
+  fish.forEach(f => {
+    g[f]++;
+  })
 
-    fish = `${ng}${'8'.repeat(n)}`;
+  for (let day = 1; day <= 256; day++) {
+    const ng = g.shift();
+    g[8] = ng;
+    g[6] += ng;
   }
-  const solution = fish.length;
+  let solution = 0;
+  g.forEach(v => {
+    solution += v
+  })
   console.log(`Solution: ${solution}`)
 });
