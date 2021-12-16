d69fe0436365f2a9c8ed4c7be31a4d46647c421b
Arthur Hoek <arthur@arthurhoek.nl>
2021-12-16 22:58:54 +0100

    Day 13 - 2

--- day13/part2.js
+++ day13/part2.js
@@ -10,11 +10,10 @@ fs.readFile(filename, 'utf8', (err, data) => {
       const [_, axis, index] = l.match(/fold along (\w)=(\d+)/)
       return { axis, index: parseInt(index)}
     })
-  let solution = 0;
-
-  dots = fold(dots, folds[0])
-  solution = dots.length
-  console.log(`Solution: ${solution}`)
+  folds.forEach(f => {
+    dots = fold(dots, f)
+  })
+  draw(dots)
 
   function fold(dots, fold) {
     const a = fold.axis === 'x' ? 0 : 1;
@@ -30,4 +29,15 @@ fs.readFile(filename, 'utf8', (err, data) => {
 
 function unique(arr) {
   return Array.from(new Set(arr.map(JSON.stringify)), JSON.parse)
+}
+
+function draw(dots) {
+  grid = []
+  for (let y = 0; y < 10; y++) {
+    grid[y] = []
+    for (let x = 0; x < 40; x++) {
+      grid[y][x] = dots.find(d => d[0] === x && d[1] === y) ? '#' : ' '
+    }
+  }
+  console.log(grid.map(l => l.join('')).join("\n"))
 }
