74518b7e3a94ee0b965660e0ecdb9a8de12094da
Arthur Hoek <arthur@arthurhoek.nl>
2021-12-16 22:08:57 +0100

    Day 12 - 2

--- day12/part2.js
+++ day12/part2.js
@@ -13,25 +13,33 @@ fs.readFile(filename, 'utf8', (err, data) => {
   console.log(`Solution: ${solution}`)
 
   function next(p1, vs, path, twice) {
+    if (twice && path.includes(p1) && isLower(p1)) {
+      return false;
+    }
     const vsN = vs.filter(v => v[0] === p1)
     let vsNew
     if (isLower(p1)) {
       if (path.includes(p1)) {
         twice = true
       }
-      vsNew = [...vs.filter(v => v[1] !== p1)]
+      if (twice) {
+        vsNew = [...vs.filter(v => v[1] !== p1)]
+      } else {
+        vsNew = [...vs]
+      }
     } else {
-      vsNew = [...vs];
+      vsNew = [...vs]
     }
     path.push(p1)
-
     vsN.forEach(v => {
       const p2 = v[1]
       if (p2 === 'end') {
+        //path.push(p2)
         console.log(path.join('-'))
         solution++
+        return
       } else {
-        next(p2, [...vsNew], [...path])
+        next(p2, [...vsNew], [...path], twice)
       }
     })
   }
