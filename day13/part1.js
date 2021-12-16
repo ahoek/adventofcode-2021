7b84e3c8b3faa2bc36b908e735df4f6360dc7666
Arthur Hoek <arthur@arthurhoek.nl>
2021-12-16 22:39:20 +0100

    Day 13 - 1

--- day13/part1.js
+++ day13/part1.js
@@ -1,7 +1,7 @@
 const fs = require('fs'), filename = process.argv[2];
 fs.readFile(filename, 'utf8', (err, data) => {
   let lines = data.trim().split("\n")
-  const dots = lines
+  let dots = lines
     .filter(l => l.includes(','))
     .map(l => l.split(',').map(x => parseInt(x)))
   const folds = lines
@@ -10,8 +10,29 @@ fs.readFile(filename, 'utf8', (err, data) => {
       const [_, axis, index] = l.match(/fold along (\w)=(\d+)/)
       return { axis, index: parseInt(index)}
     })
-  console.log(dots, folds)
+  // console.log(dots, folds)
 
   let solution = 0;
+
+  dots = fold(dots, folds[0])
+  console.log(dots);
+  solution = dots.length
   console.log(`Solution: ${solution}`)
+
+  function fold(dots, fold) {
+    console.log(fold)
+    const a = fold.axis === 'x' ? 0 : 1;
+    dots.forEach(d => {
+      if (d[a] > fold.index) {
+        console.log ('mirror', d)
+        d[a] = fold.index - (d[a] - fold.index)
+      }
+    })
+
+    return unique(dots)
+  }
 });
+
+function unique(arr) {
+  return Array.from(new Set(arr.map(JSON.stringify)), JSON.parse)
+}
